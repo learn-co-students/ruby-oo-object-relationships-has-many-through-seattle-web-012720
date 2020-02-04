088929c2b17d9063e1c8ee62175737dd8631b55f
github username <github email address>
2020-02-03 17:18:12 -0800

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Waiter
+  
+    attr_accessor :name, :years
 
+    @@all = []
+
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all<< self
+    end  
+    
+    def self.all
+        @@all
+    end
+
+    def new_meal (customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        biggie = Meal.all.sort_by {|meal| meal.tip}[-1]
+        biggie.customer
+    end
 end
